a734bd06db7f054fd5b21c9dbf4328fea82dbe28
Daniel Castiblanco <razzziel001@gmail.com>
2020-01-23 21:57:50 -0500

    Trabajando funcionalidad de carrusel y modal con boostrap

---
+++ js/carru.js
@@ -0,0 +1,20 @@
+//Esto se encarga del carrusel de imagenes
+var slideIndex = 1;
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("lista");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
--- js/request.js
+++ js/request.js
@@ -1,59 +1,132 @@
-function solicitud(){
-url = "https://jsonplaceholder.typicode.com";
-var banner = document.getElementById("banner");
-var img = document.createElement("img");
+function solicitud() {
+  //1Primero traemos el json
+  var url = "https://jsonplaceholder.typicode.com";
 
-var datos = document.createElement("div");
-var habilidad = document.createElement("h3");
+  invoVariables();
 
-var boton = document.createElement("button");
-var elejir = document.createElement("h4");
-
-var title = document.createElement("h1");
-
-
-//Performing multiple concurrent requests para utilizar nombre de  los personajes
-axios.get(url + "/photos",{
-params:{
-    _limit:5,
-    //name_like: 'cle'
-    }
-})
-.then(function (response) {
-
-  for (let i in response.data) {
-    title = document.createElement("h1");
-    boton = document.createElement("button");
-    elejir = document.createElement("h4");
-    img = document.createElement("img");
-    img.src = response.data[i].thumbnailUrl;
-
-    datos = document.createElement("div");
-    habilidad = document.createElement("h3");
-    habilidad.innerText = response.data[i].title;
-    datos.appendChild(habilidad);
-
-    banner.appendChild(datos);
-    banner.appendChild(img);
-
-    elejir.innerText = "Elejir";
-    title.innerText = "Nombre";
-    datos.appendChild(boton);
-    datos.appendChild(title);
-    boton.appendChild(elejir);
-
-
+  //2Se crean las funciones que moveran los post
+  function getUser() {
+    //Funcion que trae los usuarios
+    return axios.get(url + "/users", {
+      params: {
+        _limit: 5
+      }
+    });
   }
-    console.log(response.data);
+  function getPhotos() {
+    //Funcion que trae las fotos e imagenes
+    return axios.get(url + "/photos", {
+      params: {
+        _limit: 5
+      }
+    });
+  }
+  //3se utiliza axios.all para llamar las funciones anteriores
+  axios
+    .all([getUser(), getPhotos()])
 
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-    console.log("axios funciona!")
-  });
+    //4Usamos las promesas de axios para llamar los post
+    .then(
+      axios.spread(function(users, photos) {
+        dinamizmo();
+        function dinamizmo() {
+          //modal es la pestaña de muestra al elegir un personaje
+          var modal = document.getElementById("modal-text");
+          //Un for que recorre cada post basado en su limite
+          for (let i in users.data && photos.data) {
+            /*CREACIÓN DE ELEMENTOS DINÁMICOS*/
+            //Creo el div que contendrá carrusel
+            datos = document.createElement("div");
+            //Le creo una clase al div
+            datos.classList.add("lista");
 
+            /*Descripción general carrusel*/
+            //Creamos el h3 de la descripcion en el carrusel
+            //importamos data del json y lo volvemos hijo del div principal
+            habilidad = document.createElement("h3");
+            habilidad.innerText = photos.data[i].title;
+            datos.appendChild(habilidad);
+
+            /*Nombre personaje carrusel*/
+            //Creación del h1 que contiene nombre del personaje/usuario
+            title = document.createElement("h1");
+            title.innerText = users.data[i].username;
+            datos.appendChild(title);
+
+            /*boton de elegir*/
+            boton = document.createElement("button");
+            datos.appendChild(boton);
+
+            //Imagen de fondo carrusel por cada personaje
+            img = document.createElement("img");
+            //extraer imagenes de json
+            img.src = photos.data[i].url;
+            //Creamos un id para cada div del carrusel
+            datos.id = "background" + i;
+            //vuelvo el div hijo del id banner
+            banner.appendChild(datos);
+            //convierto las imagenes del json en el fondo del carrusel
+            datos.style.backgroundImage = "url('" + photos.data[i].url + "')";
+            //Texto del boton elegir
+            elegir = document.createElement("a");
+            //el texto del boton del carrusel
+            elegir.innerText = "Elegir";
+            //detección del id para mostrar segun el post en modal de boostrap
+            elegir.id = "elegir" + i;
+            //Muestra el modal del boostrap
+            elegir.onclick = function() {
+              //esta funcion es la que modifica la visibildiad del modal con jquery
+              $("#modal").modal("show");
+              //datos extraidos de users para una descripción del personaje elegido
+              modal.innerText =
+                users.data[i].name +
+                "\n" +
+                users.data[i].email +
+                "\n" +
+                users.data[i].phone +
+                "\n" +
+                users.data[i].website +
+                "\n" +
+                users.data[i].address.street +
+                "\n" +
+                users.data[i].address.suite +
+                "\n" +
+                users.data[i].address.city +
+                "\n" +
+                users.data[i].address.zipcode;
+            };
+
+            boton.appendChild(elegir);
+          }
+        }
+
+        showDivs(1);
+        console.log(photos.data);
+      })
+    )
+    //5Un catch por si falla el envio
+    .catch(function(error) {
+      console.log(error);
+    })
+    //6finally para confirmar que todo esta correcto
+    .finally(function() {
+      console.log("Axios Funciona!");
+    });
+}
+
+//Creamos los elementos dinamicos
+function invoVariables() {
+  //Banner será el id padre que desplegará los principales datos json
+  var banner = document.getElementById("banner");
+  var img = document.createElement("img");
+
+  var datos = document.createElement("div");
+  var habilidad = document.createElement("h3");
+
+  var boton = document.createElement("button");
+  var elegir = document.createElement("h4");
+
+  var title = document.createElement("h1");
+  return banner, img, datos, habilidad, boton, elegir, title;
 }
 solicitud();
